1ff5504762a5325b1615ac6cf5e73013906276b6
YohanCS <42155504+YohanCS@users.noreply.github.com>
2019-03-18 00:06:39 -0700

    added in body-parser and form submit

--- nodekb/app.js
+++ nodekb/app.js
@@ -2,8 +2,9 @@
 const express = require('express'); 
 const path = require('path'); // standard module we get
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-// we installed mongoose so let's connect it
+// we installed mongoose so let's connect it to mongodb's Service process
 mongoose.connect('mongodb://localhost/nodekb', {useNewUrlParser: true});
 let db = mongoose.connection;
 
@@ -13,7 +14,7 @@ db.once('open', () => {
 });
 
 // before we init app 
-// Check for DB errors
+// Check for DB errors 
 db.on('error', (err) => {
     console.log(err);
 });
@@ -27,6 +28,12 @@ let Article = require('./models/article'); // to use article variable which is a
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Body Parser Middleware to parse the form submittions later on
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
 
 // GET request to homepage
 // req = request, res = response
@@ -46,12 +53,34 @@ app.get('/', (req, res) => {
     
 });
 
+
 // add route
 app.get('/articles/add', (req, res) => {
     // render another template
     res.render('add_articles', {
         title: 'Add Article'
     });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', (req, res) => {
+    let article = new Article();
+    article.title = req.body.title;  // need to add a module called body parser
+    article.author = req.body.author;
+    article.body = req.body.body; 
+
+    // save 
+    article.save((err) => {
+        if(err) {
+            console.log(err);
+            return; 
+        }
+        // redirect to index page 
+        res.redirect('/');
+        
+    });
+
+    console.log("Submitted");
 });
 
 // need to call listen function so res.send actually does something
